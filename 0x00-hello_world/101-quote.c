ac015e8727f612fb384838b61080b6d0b73a17f1
Cyrus Waiganjo <cyruswaiganjo@gmail.com>
2023-02-26 12:14:05 -0800

    Hello, world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include stdio.h
+
+/**
+ * main-entry point
+ * return: Always 1 (Success)
+ */
+
+int main (void)
+{
+	char q[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+	
+	fprintf(stderr, "%s", q);
+	return(1);
+}
