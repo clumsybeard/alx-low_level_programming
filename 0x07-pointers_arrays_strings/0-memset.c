1f44cc9f8fcf85aa9146e3b96b040b7502cfbf88
Cyrus Waiganjo <cyruswaiganjo@gmail.com>
2023-03-07 10:46:15 -0800

    Betty betty betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * _memset - fill a bolck of memory with a specific value
- *@s: starting address of the memory to be filled
- *@b: the desired value
- *@n: the number of bytes to be changed
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
  *
- *Return: changed array with new vale for bytes
+ * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (; n>0; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
