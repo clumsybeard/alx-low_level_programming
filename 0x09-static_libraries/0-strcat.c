b70e4812c2833ef91d5775ae56b0f4e2a85748ac
Cyrus Waiganjo <cyruswaiganjo@gmail.com>
2023-03-19 06:24:33 -0700

    Betty

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -6,25 +6,24 @@
  *
  * Return: void
  */
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
 	i = 0;
-	while (dest[i]!='\0')
+	while (dest[i] != '\0')
 	{
-	i++;
+		i++;
 	}
 	j = 0;
-	while (src[j] !='\0')
+	while (src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
 	dest[i] = '\0';
 	return (dest);
 }
-
